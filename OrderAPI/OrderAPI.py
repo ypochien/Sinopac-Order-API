4461cbb09ff31339c979d82cf6f356b66f341477
ypochien <ypochien@gmail.com>
2016-12-04 13:53:39 +0800

    因應T4改為utf8版本，重構原先的部分，將編碼轉換的全部拿掉.

--- OrderAPI/OrderAPI.py
+++ OrderAPI/OrderAPI.py
@@ -1,17 +1,46 @@
 import json
 
-from OrderAPI.pyT4 import pyT4
+from OrderAPI.pyT4.pyT4 import T4
+
+ACCOUNT_TYPE = {
+    'S': '股票',
+    'F': '期貨'
+}
+
+
+class User(object):
+    def __init__(self, acc):
+        """
+        S9A95 - 9809315 - 楊伯謙
+        FF002000 - 9114728 - 楊伯謙
+        """
+        self.type = acc.split('-')[0].strip()[0]
+        self.branch = acc.split('-')[0].strip()[1:]
+        self.account = acc.split('-')[1].strip()
+        self.name = acc.split('-')[2].strip()
+
+    def __str__(self):
+        return '{}-{}{}-{}'.format(ACCOUNT_TYPE[self.type], self.branch, self.account, self.name)
 
 
 class OrderAPI(object):
+    @classmethod
+    def accounts(cls):
+        accounts = T4.show_list2()
+        accounts = [acc for acc in accounts.split('\n') if len(acc)]
+        for acc in accounts:
+            yield User(acc)
+
     def __init__(self, config_file='OrderAPI.json'):
+        self._init_t4(config_file)
+        self.accounts = [acc for acc in OrderAPI.accounts()]
+
+    def _init_t4(self, config_file):
         with open(config_file) as fd_json:
             self.UserInfo = json.load(fd_json)
-        msg = pyT4.init_t4(self.UserInfo['UserId'].encode('utf-8'), self.UserInfo['Password'].encode('utf-8'),
-                           ''.encode('utf-8'))
-        new_msg = str(msg, 'cp950')
-        self._status = new_msg
-        pyT4.do_register(1)
+        msg = T4.init_t4(self.UserInfo['UserId'], self.UserInfo['Password'], '')
+        self._status = msg
+        T4.do_register(1)
 
     @property
     def status(self):
@@ -19,15 +48,14 @@ class OrderAPI(object):
 
     @status.setter
     def status(self, value):
-        self._status = value
+        self.status = value
 
     @property
     def server_ip(self):
-        ip_port = str(pyT4.show_ip(), 'cp950')
+        ip_port = T4.show_ip()
         ip = ip_port.split('\n')[0].split(':')[1].strip()
         port = ip_port.split('\n')[1].split(':')[1].strip()
         return '{}:{}'.format(ip, port)
 
     def get_account_list(self):
-        accounts = str(pyT4.show_list2(), 'cp950')
-        return [acc for acc in accounts.split('\n') if len(acc)]
+        return self.accounts
